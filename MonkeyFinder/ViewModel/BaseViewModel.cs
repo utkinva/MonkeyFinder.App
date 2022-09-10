1b663f7a139335a6a163f3e856baf9a38a336c67
Владислав <utkinvaa@gmail.com>
2022-09-10 21:12:07 +0300

    Добавьте файлы проекта.

---
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
+{
+    public BaseViewModel()
+    {
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !isBusy;
+}
